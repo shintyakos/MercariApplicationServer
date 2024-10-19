1b6a1109a19c848ed0cb7112eb3b7f3d115281b7
shintyakos <shintyakos1145@gmail.com>
2024-10-19 09:23:28 +0900

    Initial commit

---
+++ src/main/java/com/stake/mercariapplicationserver/MercariApplicationServerApplication.java
@@ -0,0 +1,13 @@
+package com.stake.mercariapplicationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercariApplicationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MercariApplicationServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stake/mercariapplicationserver/MercariApplicationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stake.mercariapplicationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercariApplicationServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
